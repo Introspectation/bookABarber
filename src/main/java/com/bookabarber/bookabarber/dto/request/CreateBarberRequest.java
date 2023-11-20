994e234f98e0c82a3158c4a2c7b99028d7c7e8a0
Deniz Ural <denizural9@gmail.com>
2023-11-20 15:26:22 +0300

    Necessary annotations added to CreateBarberRequest

--- src/main/java/com/bookabarber/bookabarber/dto/request/CreateBarberRequest.java
+++ src/main/java/com/bookabarber/bookabarber/dto/request/CreateBarberRequest.java
@@ -4,15 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateBarberRequest {
+
+    @NotNull
+    @NotBlank
     private String barberFirstName;
     private String barberMiddleName;
+    @NotNull
+    @NotBlank
     private String barberLastName;
+    @NotNull
+    @NotBlank
     private String barberNationalIdNumber;
+    @NotNull
+    @NotBlank
     private String barberPhoneNumber;
+    @NotNull
+    @NotBlank
     private String barberEmailAddress;
+    @NotNull
+    @NotBlank
     private String barberShopName;
 }
